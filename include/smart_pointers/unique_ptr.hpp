fde8136a8032be0d1eb057dafb4503d3e672aa71
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-06-14 11:06:24 +0100

    feat : defined deleter for single objects

--- include/smart_pointers/unique_ptr.hpp
+++ include/smart_pointers/unique_ptr.hpp
@@ -1,4 +1,18 @@
-#pragma once
+#pragma once //include guard
 #include <utility>
 #include <type_traits>
 #include <cstddef>
+
+
+namespace custom {
+    //default deleter functor for single objects
+    template <typename T>
+    struct default_delete {
+        void operator()(T* ptr) const {
+            delete ptr;
+        }
+    };
+
+    //default deleter specialization for arrays
+    
+}
